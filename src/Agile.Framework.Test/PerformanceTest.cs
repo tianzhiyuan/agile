0b1e88fcbc8cbec2dd89bdfa7114672526971132
tianzhiyuan <tianzhiyuan126@126.com>
2015-02-12 12:46:14 +0800

    TODO list.txt

--- src/Agile.Framework.Test/PerformanceTest.cs
+++ src/Agile.Framework.Test/PerformanceTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Agile.Framework.Data;
@@ -16,18 +18,69 @@ namespace Agile.Framework.Test
 		public void Test()
 		{
 			var stopWatch = new Stopwatch();
-			stopWatch.Start();
 			var times = 10000;
-			DataService svc1 = new DataService()
-				{
-					ContextTypeString = ""
-				};
-			
+			DbContextType = typeof (TestDb);
+			GetContext();
+			new TestDb("test");
+			stopWatch.Start();
 			for (var index = 0; index < times; index++)
 			{
-				
+				GetContext();
 			}
 			stopWatch.Stop();
+			Console.WriteLine(stopWatch.ElapsedMilliseconds);
+		}
+		private Func<string, DbContext> _createContext;
+		private readonly object _contextCreatorLocker = new object();
+		private Type DbContextType;
+		protected DbContext GetContext()
+		{
+			Func<string, DbContext> handler = _createContext;
+			if (handler == null)
+			{
+				lock (_contextCreatorLocker)
+				{
+					if (_createContext == null)
+					{
+						var type = DbContextType;
+						if (type == null) throw new ArgumentNullException("DbContextType");
+
+						var constructors = type.GetConstructors();
+						var arg = Expression.Parameter(typeof(string));
+						var predicateMethod = typeof(string).GetMethod("IsNullOrWhiteSpace");
+						handler = _createContext = Expression.Lambda<Func<string, DbContext>>(
+							Expression.Condition(
+								Expression.Call(predicateMethod, arg),
+								Expression.New(constructors.First(o => o.GetParameters().Length == 0)),
+								Expression.New(constructors.First(o => o.GetParameters().Length == 1), arg)
+							),
+							arg
+						).Compile();
+					}
+					else
+					{
+						handler = _createContext;
+					}
+				}
+			}
+
+			var db = handler("test");
+			db.Configuration.AutoDetectChangesEnabled = false;
+			db.Configuration.ProxyCreationEnabled = false;
+			return db;
 		}
     }
+	public class TestDb: DbContext
+	{
+		public TestDb()
+			: base("Db")
+		{
+			
+		}
+
+		public TestDb(string name) : base(name)
+		{
+			
+		}
+	}
 }
